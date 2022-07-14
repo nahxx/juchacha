32a3b65065292c8da6d91ff968a67cb287ee8c0f
platformDG <diners55@naver.com>
2022-07-14 09:14:19 +0900

    22.07.14

--- chacha/src/main/java/com/teamecho/chacha/parking/servlet/GetParkingLotServlet.java
+++ chacha/src/main/java/com/teamecho/chacha/parking/servlet/GetParkingLotServlet.java
@@ -19,8 +19,6 @@ public class GetParkingLotServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ParkingLotService ps = ParkingLotService.getInstance();
 		List<ParkingLot> list = ps.findAllParkingLot();
-		//요청이 들어오면 list객체를 지도 객체로 던져준다.
-		System.out.println("진입");
 		request.setAttribute("ParkingLotList", list);
 		request.getRequestDispatcher("/location/location.jsp").forward(request, response);
 	}
