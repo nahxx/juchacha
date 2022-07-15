07f90feeff35bc634bfb88b03b4fa0acd395b58a
platformDG <diners55@naver.com>
2022-07-15 16:22:50 +0900

    22.07.15 pdg

--- chacha/src/main/java/com/teamecho/chacha/parking/servlet/GetParkingLotSerach.java
+++ chacha/src/main/java/com/teamecho/chacha/parking/servlet/GetParkingLotSerach.java
@@ -19,16 +19,11 @@ public class GetParkingLotSerach extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ParkingLotService ps = ParkingLotService.getInstance();
 		String search_str = (String) request.getParameter("keyword");
-		System.out.println(search_str);
 		List<ParkingLot> list = ps.findSearchParkingLot(search_str);
 		
 		request.setAttribute("ParkingLotList", list);
-		request.setAttribute("ON", "ON");
+		request.setAttribute("ON", "on");
 		request.getRequestDispatcher("/location/location.jsp").forward(request, response);
-		
-//		for(ParkingLot p : list) {
-//			System.out.println(p.getParkingName());
-//		}
 	}
 
 }
