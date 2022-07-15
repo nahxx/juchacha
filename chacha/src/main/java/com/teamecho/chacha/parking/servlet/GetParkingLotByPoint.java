10b3445ab0a39f3c22d205648f3f1c3ec57f1187
platformDG <diners55@naver.com>
2022-07-15 14:10:58 +0900

    pdg

--- chacha/src/main/java/com/teamecho/chacha/parking/servlet/GetParkingLotByPoint.java
+++ chacha/src/main/java/com/teamecho/chacha/parking/servlet/GetParkingLotByPoint.java
@@ -1,6 +1,7 @@
 package com.teamecho.chacha.parking.servlet;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 import javax.servlet.ServletException;
@@ -23,6 +24,15 @@ public class GetParkingLotByPoint extends HttpServlet {
 		ParkingLotService ps = ParkingLotService.getInstance();
 		HttpSession session = request.getSession(false);
 		String userId = (String) session.getAttribute("userId");
+		
+		if(userId == null || userId.length() == 0) {
+			response.setContentType("text/html; charset=UTF-8");
+			PrintWriter writer = response.getWriter();
+			writer.println("<script>alert('로그인 후 사용 해주시기 바랍니다.'); location.href='/chacha';</script>"); // 경고창 띄우기
+			writer.close(); // close를 해주면 response.reDirect가 안되므로 alert에서 location.href 속성을 사용하여 페이지를 이동시켜준다.
+			return;
+		}
+				
 		double pointx = Double.valueOf(request.getParameter("pointX"));
 		double pointy = Double.valueOf(request.getParameter("pointY"));
 		
